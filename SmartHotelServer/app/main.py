d0a2dc6de0799a4956b71192e44952e4467773de
Michael Budnikov <116641987+Mishanya00@users.noreply.github.com>
2025-05-18 03:15:52 +0300

    admin router

--- SmartHotelServer/app/main.py
+++ SmartHotelServer/app/main.py
@@ -5,6 +5,7 @@ from fastapi import FastAPI
 from app.auth.routes import auth_router
 from app.repository.init_db import create_tables
 from app.hotel.routes import hotel_router
+from app.admin.routes import admin_router
 
 
 @asynccontextmanager
@@ -16,6 +17,7 @@ app = FastAPI(lifespan=lifespan)
 
 app.include_router(auth_router, prefix="/auth", tags=["auth"])
 app.include_router(hotel_router, prefix="/hotel", tags=["hotel"])
+app.include_router(admin_router, prefix="/admin", tags=["admin"])
 
 @app.get("/")
 async def root():
